ac92cc53372c0fe01790a70585b4d12f32b22133
unknown <robeats.hale@gmail.com>
2024-11-16 00:28:59 +0100

    feat(refactor): copied original logic sequence + transferring most functions to separate file

--- main.c
+++ main.c
@@ -1,57 +1,6 @@
 #include "common.h"
 
-void	sdl_init(void)
-{
-	int	window_flags;
-	int	renderer_flags;
-	
-	window_flags = 0;
-	renderer_flags = SDL_RENDERER_ACCELERATED;
-	if (SDL_InitSubsystem(SDL_INIT_VIDEO) != 0)
-	{
-		printf("Couldn't Init SDL: %s\n", SDL_GetError());
-		exit (1);
-	}
-
-
-	app.window = SDL_CreateWindow("Gold Rush", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, window_flags);
-	if (!app.window)
-	{
-		printf("Failed to open %d x %d window: %s\n", SDL_GetError();
-		exit (1);
-	}
-
-	SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "linear");
-
-	app.renderer = SDL_CreateRenderer(app.window, 1, renderer_flags);
-	if(!app.renderer)
-	{
-		printf("Failed to create Renderer: %s\n", SDL_GetError());
-		exit (1);
-	}
-}
-
-void init_game(t_game *game)
-{
-	game->max_x = 0;
-	game->player_y = 0;
-	game->player_x = 0;
-	game->enemy_h_x = 0;
-	game->enemy_h_y = 0;
-	game->enemy_v_x = 0;
-	game->enemy_v_y = 0;
-	game->direction_v = 1;
-	game->direction_h = 1;
-	game->enemy_h_dead = 0;
-	game->enemy_v_dead = 0;
-	game->highscore = 0;
-	game->coins_highscore = 0;
-	game->kill_highscore = 0;
-	game->step_counter = 0;
-	game->gold_rush = 10;
-}
-
-void ft_title_screen(void)
+void	ft_title_screen(void)
 {
 	printf("    ▗▄▄▖ ▗▄▖ ▗▖   ▗▄▄▄ \n");
 	printf("   ▐▌   ▐▌ ▐▌▐▌   ▐▌  █ \n");
@@ -69,17 +18,38 @@ void ft_title_screen(void)
 	     unless I sneak up behind 'em!\n\n");
 }
 
+void	cleanup(void)
+{
+	if (app.window)
+		SDL_DestroyWindow(app.window);
+	if (app.renderer)
+		SDL_DestroyRenderer(app.renderer);
+	SDL_Quit();
+}
+
 int	main(int argc, char *argv[])
 {
-	t_game game;
-	t_app app;
+	t_game	game;
+	t_app	app;
 
-	memset(&app, 0, sizeof(app));
-
-	initSDL();
 	if (argc != 2)
 		return (1);
+	memset(&app, 0, sizeof(app));
+	init_sdl();
+
+	atexit(cleanup);
+
 	ft_title_screen();
 	init_game(&game);
+	if (init_map(&game, argv[argc - 1]))
+		return (1);
+
+	find_positions(&game));
+	find_positions_y(&game);
+	find_positions_x(&game);
+	count_coins(&game);
+	load_game_assets(&game);
+	start_game(&game);
+	free(game.map);
 	return (0);
 }
